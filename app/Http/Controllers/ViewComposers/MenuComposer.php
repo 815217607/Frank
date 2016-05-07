3f2db1122443a5a1d4a8bd40d117900e3d6d2dff
fuqixue <815217607@qq.com>
2016-05-07 17:38:01 +0800

    权限 超级管理员

--- app/Http/Controllers/ViewComposers/MenuComposer.php
+++ app/Http/Controllers/ViewComposers/MenuComposer.php
@@ -69,12 +69,14 @@ class MenuComposer
         }
             $psers= Permission::query()->where(function($query)use($permissions,$roles){
                 $query->whereExists(function($query) use($roles){
-                    get_class($query);
+//                    get_class($query);
                     $query->select(DB::raw(1))
                         ->from('permission_role')
                         ->whereRaw('permission_role.permission_id = permissions.id');
+                    if(!access()->allow('user-view-management'))
                     $query->whereIn('role_id',$roles);
                 });
+                if(!access()->allow('user-view-management'))
                 $query->OrwhereIn('permissions.id',$permissions);
 
             })->join('menus','menus.permission_id','=','permissions.id')->orderBy('menus.sort','desc')->get(['menus.*','permissions.name as permission_name'])->toArray();
